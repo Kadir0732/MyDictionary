2929fa4be3b0af97485f0a6c43c207ff70c099e8
Kadir0732 <66407151+Kadir0732@users.noreply.github.com>
2021-01-27 21:02:29 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+        public void add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempKey = keys;
+            Tvalue[] tempValue = values;
+        
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+
+            for(int i = 0;i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+
+            for(int j = 0;j < tempValue.Length; j++)
+            {
+                values[j] = tempValue[j];
+            }
+            values[values.Length - 1] = value;
+            keys[keys.Length - 1] = key;
+        }
+        public void show()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {         
+                    Console.WriteLine("Key: "+ keys[i]+", value: "+ values[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.add(1, "naber");
+            myDictionary.add(2, "iyi");
+            myDictionary.show();
+            Console.ReadKey();
+        }
+    }
+}
